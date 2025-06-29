09d6a4c1a0614378a0f5e268092a5a03fb659d6d
Sir Pineapple <dhruvgupta376@gmail.com>
2025-06-30 02:26:06 +0530

    add skeleton routes

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const port = 3000;
 
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = "";
+
+app.get("/", (req, res) => {
+})
+
+app.post("/user/signup", (req, res) => {
+})
+
+app.post("/user/signin", (req, res) => {
+})
+
+app.get("/courses", (req, res) => {
+})
+
+app.get("/user/purchases", (req, res) => {
+})
+
+app.post("/course/purchase", (req, res) => {
+})
+
+app.listen(port);
