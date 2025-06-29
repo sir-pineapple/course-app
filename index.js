fa20e47b0725d211cb5cd70569e9198bb096100c
Sir Pineapple <dhruvgupta376@gmail.com>
2025-06-30 03:17:24 +0530

    added routers

--- index.js
+++ index.js
@@ -2,25 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = "";
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
 
-app.get("/", (req, res) => {
-})
-
-app.post("/user/signup", (req, res) => {
-})
-
-app.post("/user/signin", (req, res) => {
-})
-
-app.get("/courses", (req, res) => {
-})
-
-app.get("/user/purchases", (req, res) => {
-})
-
-app.post("/course/purchase", (req, res) => {
-})
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
 
 app.listen(port);
---
+++ routes/course.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const courseRouter = Router();
+
+courseRouter.post("/purchase", (req, res) => {
+})
+
+courseRouter.get("/preview", (req, res) => {
+})
+
+module.exports = {
+    courseRouter: courseRouter
+}
